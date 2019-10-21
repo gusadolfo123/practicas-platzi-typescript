d9ea30ba3e95057f0943cff28578affe4d1c22c5
gusadolfo123 <gusadolfo123@hotmail.com>
2019-10-20 22:24:26 -0500

    Ajustes

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import MediaPlayer from './mediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import AdsPlugin from './plugins/Ads/index';
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause()] });
+const player = new MediaPlayer({
+	el: video,
+	plugins: [new AutoPlay(), new AutoPause(), new AdsPlugin()],
+});
 
 const buttonPlay: HTMLElement = document.querySelector('.button-play');
 buttonPlay.onclick = () => player.togglePlay();
